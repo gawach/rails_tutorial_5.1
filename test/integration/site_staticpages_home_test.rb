e72010b684e4674180e0c3077d3eb6579bf18c64
gawach <gawa9.1998@gmail.com>
2019-11-04 12:35:17 +0900

    Add validates

---
+++ test/integration/site_staticpages_home_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class SiteStaticPagesHomeTest < ActionDispatch::IntegrationTest
+  # test "home links" do
+  #   get signup_path
+  #   assert_select "a[href=?]",signup_path
+  #   assert_select "title", full_title("Sign up")
+  # end
+end
