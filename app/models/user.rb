b034f4d3443c1551b486acc26d4f94ce70db79b8
gawach <gawa9.1998@gmail.com>
2019-11-05 18:51:52 +0900

    End lesson 9.1.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+
+  attr_accessor :remember_token
+
   before_save {email.downcase!}
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -7,11 +10,24 @@ class User < ApplicationRecord
             uniqueness: {case_sensitive: false}
   has_secure_password
   validates :password, presence: true, length: {minimum: 6}
+  
+  class << self
+    #def User.digest(string)
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? 
+      BCrypt::Engine::MIN_COST :
+      BCrypt::Engin.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
 
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? 
-    BCrypt::Engine::MIN_COST :
-    BCrypt::Engin.cost
-    BCrypt::Password.create(string, cost: cost)
+    #def User.new_token
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+
+    def remember
+      self.remember_token = User.new_token
+      update_attribute(:remember_digest, User.digest(remember_token))
+    end
   end
 end
